114c3f4f5f4c3fadf37ebae3c0e6b753e7eb88e4
dieramirezma.dev <dieramirezma.dev@gmail.com>
2025-11-17 13:51:51 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from textx import metamodel_from_file
+from textx.export import model_export
+import os
+
+# Carga la gramática
+mm = metamodel_from_file("metamodel-latest.tx")
+
+# Carga el modelo (instancia)
+model = mm.model_from_file("rts.arch")
+
+print("Modelo cargado correctamente:", model)
+
+# modelo a un archivo Graphviz (.dot)
+output_dot = "modelo.dot"
+model_export(model, output_dot)
+print(f"Modelo exportado a '{output_dot}'")
+
+# .dot a una imagen PNG usando Graphviz
+output_png = "modelo.png"
+os.system(f"dot -Tpng {output_dot} -o {output_png}")
+
+if os.path.exists(output_png):
+    print(f"Imagen generada: {output_png}")
+    print(f"open {output_png}  # (en macOS)")
+else:
+    print("No se pudo generar la imagen. ¿Tienes Graphviz instalado?")
